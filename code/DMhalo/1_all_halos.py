05b721f3039644d76beba3e225235271b230e44f
yqiaorong <qiaorong.yu@balliol.ox.ac.uk>
2024-05-01 15:22:27 -0400

    new

--- code/DMhalo/1_all_halos.py
+++ code/DMhalo/1_all_halos.py
@@ -14,6 +14,6 @@ for halo_idx in range(Ngroups_Total):
     if not os.path.exists(f'result/DM_halo_density_profiles/snap_{snapNum}/halo_{halo_idx}.npy'):
         os.system(f'python3 code/DMhalo/one_halo.py --halo_idx {halo_idx}')
     else:
-        print(f'At snap {snapNum}, DM halo local index {halo_idx+1}/{Ngroups_Total} is done already.')
+        print(f'At snap {snapNum}, DM halo local index {halo_idx+1}/{Ngroups_Total} already exists.')
 
 print(f'All halos at snap {snapNum} are finished.')
