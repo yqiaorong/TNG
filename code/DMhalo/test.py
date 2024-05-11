88674e5d0a7f6881b740318e12b797b060e09bdd
Qiaorong Yu <sqyu@boslogin01.rc.fas.harvard.edu>
2024-05-11 15:09:14 -0400

    new

--- code/DMhalo/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-radial_bins = np.logspace(0,np.log10(3*1),10)
-radial_centers = 10**( (np.log10(radial_bins[1:])+np.log10(radial_bins[:-1]))/2 )
-
-print(radial_bins)
-print(radial_centers)
