bd88826f9a7790bae32c138b39fd5619f4bcfafc
Qiaorong Yu <sqyu@boslogin01.rc.fas.harvard.edu>
2024-05-19 10:05:08 -0400

    new

--- code/DMhalo/2_stacks.py
+++ code/DMhalo/2_stacks.py
@@ -9,15 +9,28 @@ args = parser.parse_args()
 
 snaps = [25, 33, 40, 50, 67, 78, 99]
 
-### Bin 3 - 3.5
-# Single stack
-for s in snaps:
-    os.system(f'python3 code/DMhalo/stacked_density_profiles.py --boxsize {args.boxsize} --res {args.res} --snap {s} --bin_start 3 --bin_end 3.5')
-# Profile time evolution
-os.system(f'python3 code/DMhalo/profiles_time_evolution.py --boxsize {args.boxsize} --res {args.res} --bin_start 3 --bin_end 3.5')
+# ### Bin 3 - 3.5
+# # Single stack
+# for s in snaps:
+#     os.system(f'python3 code/DMhalo/stacked_density_profiles.py'+
+#               f' --boxsize {args.boxsize} --res {args.res} --snap {s}'+
+#               f' --bin_start 3 --bin_end 3.5'+
+#               f' --root_dir DMhalo_density_profiles_old')
+# # Profile time evolution
+# os.system(f'python3 code/DMhalo/profiles_time_evolution.py'+
+#           f' --boxsize {args.boxsize} --res {args.res}'+
+#           f'--bin_start 3 --bin_end 3.5'+
+#           f' --root_dir DMhalo_density_profiles_old')
 
 ### Bin 3.5 - 4
+# Single stack
 for s in snaps[1:]:
-    os.system(f'python3 code/DMhalo/stacked_density_profiles.py --boxsize {args.boxsize} --res {args.res} --snap {s} --bin_start 3.5 --bin_end 4')
+    os.system(f'python3 code/DMhalo/stacked_density_profiles.py'+
+              f' --boxsize {args.boxsize} --res {args.res} --snap {s}'+
+              f' --bin_start 3.5 --bin_end 4'+
+              f' --root_dir DMhalo_density_profiles_old')
 # Profile time evolution
-os.system(f'python3 code/DMhalo/profiles_time_evolution.py --boxsize {args.boxsize} --res {args.res} --bin_start 3.5 --bin_end 4')
+os.system(f'python3 code/DMhalo/profiles_time_evolution.py'+
+          f' --boxsize {args.boxsize} --res {args.res}'+
+          f' --bin_start 3.5 --bin_end 4'+
+          f' --root_dir DMhalo_density_profiles_old')
