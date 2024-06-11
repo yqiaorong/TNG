427cab62b3caeaa3178e440a6579dfa7801daf0a
Qiaorong Yu <sqyu@boslogin02.rc.fas.harvard.edu>
2024-06-11 13:59:41 -0400

    new

--- code/DMhalo/2_stacks.py
+++ code/DMhalo/2_stacks.py
@@ -8,23 +8,8 @@ parser.add_argument('--res',default=1250,type=int)
 args = parser.parse_args()
 
 snaps = [17, 21, 25, 33, 40, 50, 67, 78, 99]
+bins = [1.5, 2, 2.5, 3, 3.5]
 
-# ### Bin 3 - 3.5
-# # Single stack
-# for s in snaps:
-#     os.system(f'python3 code/DMhalo/stacked_density_profiles.py'+
-#               f' --boxsize {args.boxsize} --res {args.res} --snap {s}'+
-#               f' --bin_start 3 --bin_end 3.5'+
-#               f' --root_dir DMhalo_density_profiles_old')
-# # Profile time evolution
-# os.system(f'python3 code/DMhalo/profiles_time_evolution.py'+
-#           f' --boxsize {args.boxsize} --res {args.res}'+
-#           f'--bin_start 3 --bin_end 3.5'+
-#           f' --root_dir DMhalo_density_profiles_old')
-
-### Bin 3.5 - 4
-
-bins = [3.5]
 for i in range(len(bins)):
     # Single stack
     for s in snaps:
