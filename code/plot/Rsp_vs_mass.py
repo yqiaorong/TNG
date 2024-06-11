271507439eb7f8e31c895010054ed0caa7d932e2
Qiaorong Yu <sqyu@boslogin02.rc.fas.harvard.edu>
2024-06-11 13:51:54 -0400

    new

--- code/plot/Rsp_vs_mass.py
+++ code/plot/Rsp_vs_mass.py
@@ -2,6 +2,7 @@ import h5py
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+from func import bootstrap
 
 root_dir = 'DMhalo_density_profiles_old'
 boxsize = 205
@@ -29,15 +30,34 @@ with h5py.File(os.path.join(data_path,'DMhalo_profiles.hdf5'), 'r') as data_f:
     
     # Iterate over snapshots
     for s, c in zip(snaps, colours):
-        # Redshfit
+        # Import data
         z = np.round(data_f[f'{s}/z'], 3)
-        # Index of Rsp
-        indices = np.argmin(data_f[f'{s}/slopes_fit'], axis=1)
-        # Rsp
-        Rsp = [data_f[f'{s}/radius_fit'][i, idx] for i, idx in enumerate(indices)] # [kpc]
+        slopes_fit = data_f[f'{s}/slopes_fit']
+        radius_fit = data_f[f'{s}/radius_fit']
+ 
+        # Bootstrap the gradients
+        num_stacks = slopes_fit.shape[0]
+        boots_grads = np.array([bootstrap(slopes_fit[stack_idx], np.min, 100) 
+                         for stack_idx in range(num_stacks)])
+        # Select 16, 50, 84 percentiles of gradients
+        final_grads = np.array([np.percentile(boots_grads[stack_idx], [16, 50, 84]) 
+                         for stack_idx in range(num_stacks)])
+        # Find Rsp with errors [kpc]
+        differ = np.array([np.array([np.abs(slopes_fit[stack_idx]-final_grads[stack_idx, i]) 
+                            for stack_idx in range(num_stacks)]) for i in range(3)])
+        Rsp_idx = np.array([np.array([np.argmin(differ[i, stack_idx]) 
+                            for stack_idx in range(num_stacks)]) for i in range(3)])
+        Rsp_with_errs = np.array([np.array([radius_fit[stack_idx, Rsp_idx[i, stack_idx]] 
+                                 for stack_idx in range(num_stacks)]) for i in range(3)]).T
         
         # Plot
-        axs.plot(mass_bins[:len(Rsp)], Rsp, color=c, label=f'z = {z}')
+        axs.errorbar(mass_bins[:num_stacks], Rsp_with_errs[:,1], 
+                     yerr = [abs(Rsp_with_errs[:,1]-Rsp_with_errs[:,0]), 
+                             abs(Rsp_with_errs[:,2]-Rsp_with_errs[:,1])], 
+                     color=c, label=f'z = {z}')
+        # axs.plot(mass_bins[:num_stacks], Rsp_with_errs[:,1], color=c, label=f'z = {z}')
+        # axs.fill_between(mass_bins[:num_stacks], Rsp_with_errs[:,0], Rsp_with_errs[:,2],
+        #                  alpha= 0.5, color=c, label=f'z = {z}')
         
 axs.set_xlabel('Mass [MSun/h]')
 axs.set_ylabel(r"$R_{sp}$[kpc]")
