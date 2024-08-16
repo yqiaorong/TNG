c8c9e90677ea4c19e18339ab54248103974f69ff
Qiaorong Yu <sqyu@boslogin04.rc.fas.harvard.edu>
2024-08-15 23:17:33 -0400

    new

--- code/plot/plot_tgt_mass.py
+++ code/plot/plot_tgt_mass.py
@@ -18,11 +18,15 @@ print('')
 
 feats = ['Rsp', 'depth', 'width_dimless', 'width_phys']
 feat_idx = args.feat_idx
+print(feats[feat_idx])
 
 root_dir = 'result/bootstrap_stats/'
 
 # Set up the plot
 fig, axs = plt.subplots(1, 1, dpi=500)
+
+# Saved data
+saved_x1, saved_x2, saved_y, saved_y_max, saved_y_min = [], [], [], [], []
 
 ##############################################################################################
 ### Plot TNG300 ### 
@@ -47,6 +51,13 @@ for isnap, snap in enumerate(TNG300_snaps):
     axs.fill_between(TNG300_mass_cuts[:num_cut], data[:, feat_idx, 0], data[:, feat_idx, 2], 
                      color=TNG300_cmap(isnap / len(TNG300_snaps)), alpha=0.2)
     
+    # Append to saved data
+    saved_x1.append(TNG300_mass_cuts[:num_cut])
+    saved_x2.append([z]*len(TNG300_mass_cuts[:num_cut]))
+    saved_y.append(data[:, feat_idx, 1])
+    saved_y_min.append(data[:, feat_idx, 0])
+    saved_y_max.append(data[:, feat_idx, 2])
+
 ##############################################################################################
 ### Plot MTNG-DM ### 
 ##############################################################################################
@@ -72,6 +83,15 @@ for isnap, snap in enumerate(MTNG_DM_snaps):
              color=MTNG_DM_cmap(isnap / len(TNG300_snaps)), label=f'z = {np.round(z, 1)}')
     axs.fill_between(MTNG_DM_mass_cuts[:num_cut], data[:, feat_idx, 0], data[:, feat_idx, 2], 
                      color=MTNG_DM_cmap(isnap / len(TNG300_snaps)), alpha=0.2)
+    
+    # Append to saved data
+    saved_x1.append(MTNG_DM_mass_cuts[:num_cut])
+    saved_x2.append([z]*len(MTNG_DM_mass_cuts[:num_cut]))
+    saved_y.append(data[:, feat_idx, 1])
+    saved_y_min.append(data[:, feat_idx, 0])
+    saved_y_max.append(data[:, feat_idx, 2])
+
+
 
 # Final edit
 axs.set_xscale('log')
@@ -84,7 +104,7 @@ elif feat_idx == 1:
 elif feat_idx == 2:
     axs.set_ylabel(r'width [$R_{200}$]')
 elif feat_idx == 3:
-    axs.set_ylabel(r"Wdith [kpc]")
+    axs.set_ylabel(r"wdith [kpc]")
     axs.set_yscale('log')
 axs.legend()
 
@@ -94,3 +114,34 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 plt.savefig(f'{save_dir}/dm_{feats[feat_idx]}_vs_mass')
 plt.close()
+
+
+
+# Flatten the data
+saved_x1 = np.array(np.concatenate(saved_x1).tolist())
+saved_x2 = np.array(np.concatenate(saved_x2).tolist())
+saved_y  = np.array(np.concatenate(saved_y).tolist())
+saved_y_min = np.array(np.concatenate(saved_y_min).tolist())
+saved_y_max = np.array(np.concatenate(saved_y_max).tolist())
+
+# Curve fit
+from scipy.optimize import curve_fit
+
+def model_func(x, a, b, c, d, e, f):
+    logM, z = x
+    return a*logM**2 + b*logM*z + c*z**2 + d*logM + e*z + f
+
+x = np.vstack((np.log10(saved_x1), saved_x2))
+if feat_idx == 0 or 3:
+    param, pcov = curve_fit(model_func, x, np.log10(saved_y), 
+                            sigma=(np.log10(saved_y_max)-np.log10(saved_y_min))/2, 
+                            absolute_sigma=True)
+    print(f'func: log SP (logM, z) = {param[0]:.3f} (log M)^2 + {param[1]:.3f} (log M) z '+
+      f'+ {param[2]:.3f} z^2 + {param[3]:.3f} logM + {param[4]:.3f} z + {param[5]:.3f}')
+else:
+    param, pcov = curve_fit(model_func, x, saved_y, 
+                            sigma=(saved_y_max-saved_y_min)/2, 
+                            absolute_sigma=True)
+    print(f'func: SP (logM, z) = {param[0]:.3f} (log M)^2 + {param[1]:.3f} (log M) z '+
+        f'+ {param[2]:.3f} z^2 + {param[3]:.3f} logM + {param[4]:.3f} z + {param[5]:.3f}')
+print(param)
