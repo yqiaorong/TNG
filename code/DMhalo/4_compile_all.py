e36a777a5fd80183f0d8e14eebaf9d53d5757992
Qiaorong Yu <sqyu@holylogin01.rc.fas.harvard.edu>
2024-08-30 22:05:17 -0400

    new

--- code/DMhalo/4_compile_all.py
+++ code/DMhalo/4_compile_all.py
@@ -6,4 +6,5 @@ mass_bins = np.arange(1, 4.5, 0.5)
 
 for snap in snaps:
     for bin_start in mass_bins:
-        os.system(f'python3 code/DMhalo/compile_chunk_profile.py --snapnum {snap} --bin_start {bin_start} --bin_end {bin_start+0.5}')
+        if not os.path.exists(f'result/DMhalo_density_profiles/TNG300/sim_205_1250/snap_{snap}/final_densities/bin-{int(bin_start*10)}-{int(bin_start*10+5)}.npy'):
+            os.system(f'python3 code/DMhalo/compile_chunk_profile.py --snapnum {snap} --bin_start {bin_start} --bin_end {bin_start+0.5}')
