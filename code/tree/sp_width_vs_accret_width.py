9c8f01d2530dced72b43e49a3012d6cfda2ef0d3
Qiaorong Yu <sqyu@holylogin05.rc.fas.harvard.edu>
2024-09-20 17:18:39 -0400

    new

--- code/tree/sp_width_vs_accret_width.py
+++ code/tree/sp_width_vs_accret_width.py
@@ -1,16 +1,23 @@
 import numpy as np
 import os
+import seaborn as sns
+from scipy.stats import pearsonr
 from matplotlib import pyplot as plt 
 plt.style.use('code/style.mplstyle')
 
-DM = '_DM'
+DM = '' # [_DM / ]
+sim = 'TNG300' # [ TNG300 / MTNG]
 snap_list = [8, 13, 25, 40, 67, 99]
 acc_width_type = 'percentile' # [std / percentile]
 
 
 # Load accretion rate width
 root_dir = 'result/accretion_rate_plot/'
-acc_data = np.load(f'{root_dir}/sim_205_1250{DM}/TNG300{DM}_accret_stats.npy', allow_pickle=True).item()
+if sim == 'TNG300': 
+    acc_data = np.load(f'{root_dir}/sim_205_1250{DM}/TNG300{DM}_accret_stats.npy', allow_pickle=True).item()
+elif sim == 'MTNG':
+    acc_data = np.load(root_dir, allow_pickle=True).item()
+    
 redshifts = acc_data['redshifts']
 # print('redshift: ', np.round(redshifts, 3))
 mass_cuts = acc_data['mass_cuts'] # [1.  1.5 2.  2.5 3.  3.5 4. ]
@@ -29,37 +36,40 @@ elif acc_width_type == 'percentile':
 Nboots = 1024
 feat_idx = 2 # width
 root_dir = 'result/bootstrap_stats/'
-TNG300_dir = f'{root_dir}/TNG300/sim_205_1250{DM}/Nboots_{Nboots}/'
-TNG300_list = os.listdir(TNG300_dir)
+if sim == 'TNG300': 
+    sp_dir = f'{root_dir}/TNG300/sim_205_1250{DM}/Nboots_{Nboots}/'
+elif sim == 'MTNG' and DM == '_DM':
+    sp_dir = f'{root_dir}/MTNG/DM-Arepo/MTNG-L500-4320-A/output/Nboots_{Nboots}/'
+elif sim == 'MTNG' and DM == '':
+    sp_dir = f'{root_dir}/MTNG/Hydro-Arepo/MTNG-L500-4320-A/output/Nboots_{Nboots}/'
+TNG_list = os.listdir(sp_dir)
 
-# TNG300_z = []
-TNG300_all_data = []
+TNG_all_data = []
 for snap in snap_list: # from low z to high z (present)
-        
     # Load data
-    data = np.load(TNG300_dir+f'/snap_{snap}_Rsp_stats.npy', allow_pickle=True).item()
+    data = np.load(sp_dir+f'/snap_{snap}_Rsp_stats.npy', allow_pickle=True).item()
     z = data['z']
-    # TNG300_z.append(z)
     data = data['final_results']
-    TNG300_all_data.append(data)
+    TNG_all_data.append(data)
 # print('Sanity check of redshifts: ', np.round(TNG300_z, 3))
 
 
 
 # Set up the plot
-fig, axs = plt.subplots(1, 1, dpi=500)
+sns.set(style="whitegrid")
+fig, axs = plt.subplots(1, 1, dpi=500, figsize=(5, 4))
 if DM == '':
     cmap_name = 'autumn'
 elif DM == '_DM':
     cmap_name = 'winter'
-cmap = plt.get_cmap(cmap_name, len(TNG300_list))
+cmap = plt.get_cmap(cmap_name, len(TNG_list))
 
 
-
+tot_plot_x, tot_plot_y = [], []
 for cut_idx in range(len(mass_cuts)-1): # from low cut to high cut
     mask = median_array[:, cut_idx] != 0
     plot_x = width_array[mask, cut_idx]
-    all_data = [d for d, m in zip(TNG300_all_data, mask) if m==True]
+    all_data = [d for d, m in zip(TNG_all_data, mask) if m==True]
 
     # Sort all data
     plot_y, plot_y_min, plot_y_max = [], [], []
@@ -71,17 +81,34 @@ for cut_idx in range(len(mass_cuts)-1): # from low cut to high cut
             plot_y_max.append(all_data[snap_idx][cut_idx, feat_idx, 2])
         else:
             pass
+        
+    axs.scatter(plot_x[np.argsort(plot_x)], np.array(plot_y)[np.argsort(plot_x)], 
+                color=cmap(cut_idx / len(mass_cuts)),
+                label=r'$10^{%.1f}$'%mass_cuts[cut_idx]+'~'
+                +r'$10^{%.1f}$ '%mass_cuts[cut_idx+1]+'$M_\\odot$/h', 
+                s=10)
+    # axs.plot(plot_x[np.argsort(plot_x)], np.array(plot_y)[np.argsort(plot_x)], color=cmap(cut_idx / len(mass_cuts)),
+    #         label=r'$10^{%.1f}$'%mass_cuts[cut_idx]+'~'
+    #         +r'$10^{%.1f}$ '%mass_cuts[cut_idx+1]+'$M_\\odot$/h', 
+    #         linestyle='--')
+    # axs.fill_between(width_array[:len(plot_y), cut_idx], plot_y_min, plot_y_max, 
+    #                 color=cmap(cut_idx / len(mass_cuts)), alpha=0.2)
+    
+    tot_plot_x.append(plot_x[np.argsort(plot_x)])
+    tot_plot_y.append(np.array(plot_y)[np.argsort(plot_x)])
+tot_plot_x = [item for sublist in tot_plot_x for item in sublist]
+tot_plot_y = [item for sublist in tot_plot_y for item in sublist]
 
-    axs.scatter(plot_x[np.argsort(plot_x)], np.array(plot_y)[np.argsort(plot_x)], color=cmap(cut_idx / len(mass_cuts)),
-        # label=r'$10^{%.1f}$'%mass_cuts[cut_idx]+'~'
-        # +r'$10^{%.1f}$ '%mass_cuts[cut_idx+1]+'$M_\\odot$/h', 
-        s=10)
-    axs.plot(plot_x[np.argsort(plot_x)], np.array(plot_y)[np.argsort(plot_x)], color=cmap(cut_idx / len(mass_cuts)),
-            label=r'$10^{%.1f}$'%mass_cuts[cut_idx]+'~'
-            +r'$10^{%.1f}$ '%mass_cuts[cut_idx+1]+'$M_\\odot$/h', 
-            linestyle='--')
-#     # axs.fill_between(width_array[:len(plot_y), cut_idx], plot_y_min, plot_y_max, 
-#     #                 color=cmap(cut_idx / len(mass_cuts)), alpha=0.2)
+
+
+r_value, p_value = pearsonr(tot_plot_x, tot_plot_y)
+if DM == '_DM':
+    reg_color = "blue"
+elif DM == '':
+    reg_color = "red"
+axs = sns.regplot(x=tot_plot_x, y=tot_plot_y, ci=95, 
+                  scatter=False, line_kws={"color": reg_color})
+plt.title(f'r = {r_value:.2f}, p = {p_value:.3f}', loc='center')
 
 # Final edit
 axs.set_xlabel(f'Accretion rate width ({acc_width_type})')
@@ -92,5 +119,5 @@ axs.legend()
 save_dir = f'result/width/TNG300{DM}/'
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
-plt.savefig(os.path.join(save_dir, f'TNG300{DM}_sp_width_accret_width_{acc_width_type}'))
+plt.savefig(os.path.join(save_dir, f'{sim}{DM}_sp_width_accret_width_{acc_width_type}'))
 plt.close()
