df6c0b21bb906cdf3ff6f694accb74f41c32ed4e
Qiaorong Yu <sqyu@boslogin08.rc.fas.harvard.edu>
2025-02-19 17:38:48 -0500

    add formation time

--- code/DMhalo_TNG300/5_add_formation_time.py
+++ code/DMhalo_TNG300/5_add_formation_time.py
@@ -1,3 +1,47 @@
 """All the data computed in this script is saved in result/DMhalo_density_profiles"""
 
+import os
+import argparse
+import pandas as pd
 import numpy as np
+from accret_func import *
+
+# Input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--sim',    default='TNG300/sim_205_1250_DM/', type=str)
+parser.add_argument('--snapnum',default=None, type=int)
+args = parser.parse_args()
+
+print('')
+print(f'>>> Add formation time to halo data <<<')
+print('\nInput arguments:')
+for key, val in vars(args).items():
+	print('{:16} {}'.format(key, val))
+print('')
+
+
+# Load the index table
+# ------------------------------------------------------------------------------------------------
+idx_table_dir = f'result/DMhalo_table_mass/{args.sim}/snap_idx_table.csv'
+df_idx = pd.read_csv(idx_table_dir, index_col=0)
+# Convert non nan entries in df_idx to int
+df_idx = df_idx.fillna(-1)
+df_idx = df_idx.astype(int) 
+df_idx = df_idx.replace(-1, np.nan)
+
+
+# Load the mass data
+# ------------------------------------------------------------------------------------------------
+mass_table_dir = f'result/DMhalo_table_mass/{args.sim}/mass_table.csv'
+df_mass = pd.read_csv(mass_table_dir, index_col=0)
+
+# Sort the mass so it matches the column of the index table
+df_mass_sort = df_mass[df_idx.columns]
+# Check if all of two columns are equal
+print(np.all(df_mass_sort.columns == df_idx.columns))
+del df_mass
+
+
+# Add the formation time to the snap data
+# ------------------------------------------------------------------------------------------------
+add_formation_time(args.sim, args.snapnum, df_idx, df_mass_sort)
--- code/DMhalo_TNG300/accret_func.py
+++ code/DMhalo_TNG300/accret_func.py
@@ -89,3 +89,111 @@ def add_accret(sim_type, snapnum, snap_idx_table, snap_accret_table):
     np.save(os.path.join(save_dir, f'bin-{int(bin_starts[0]*10)}-{int(bin_ends[-1]*10)}'), save_dict)
     
     return save_dir
+
+def add_formation_time(sim, snapnum, idx_table, mass_table):
+    import os
+    import h5py
+    import numpy as np
+    from tqdm import tqdm
+    import illustris_python as il
+    
+    # Load source data
+    # -------------------------------------------------------------------------------------------------
+    if 'DM' in sim:
+        basePath =  '/n/holylfs05/LABS/hernquist_lab/IllustrisTNG/Runs/L%dn%dTNG'%(205,1250)+'_DM/output/'
+    elif 'Hydro' in sim:
+        basePath =  '/n/holylfs05/LABS/hernquist_lab/IllustrisTNG/Runs/L%dn%dTNG'%(205,1250)+'/output/'
+    snap_all_mass = il.groupcat.loadHalos(basePath, snapnum, fields='Group_M_Mean200')
+    
+    # Load all redshifts
+    snap_list = idx_table.index.tolist()
+    z_dict = {}
+    for snap in snap_list:
+        with h5py.File(il.snapshot.snapPath(basePath, int(snap[5:])), 'r') as f:
+            header = dict(f['Header'].attrs.items())
+            scale_factor = header['Time']
+            z = 1 / scale_factor - 1
+            z_dict[snap] = z
+    
+    # Load halo density profile data
+    # -------------------------------------------------------------------------------------------------
+    halos_dir = f'result/DMhalo_density_profiles/{sim}/snap_{snapnum}/final_densities/'
+    halos_fname = os.listdir(halos_dir)[0]
+    data = np.load(os.path.join(halos_dir, halos_fname), allow_pickle=True).item()
+    
+    # Check halo numbers
+    # -------------------------------------------------------------------------------------------------
+    # Get the bin starts and ends
+    bin_start = float(halos_fname.split('-')[1])/10 
+    bin_end = float(halos_fname.split('-')[2].split('.')[0])/10
+    print(bin_start, bin_end)
+ 
+    # Get the halo snap indices
+    subset_idx = np.where((snap_all_mass >= 10**bin_start) & (snap_all_mass < 10**bin_end))[0]
+    print(subset_idx.shape, data['halo_M_Mean200'].shape)
+    # Check if the data shape matches
+    if subset_idx.shape != data['halo_M_Mean200'].shape:
+        # Stop the script
+        os._exit(0)
+    else:
+        pass
+        
+    # Add the corresponding formation time
+    # -------------------------------------------------------------------------------------------------
+    # These two snapshots are two early and the results would be meaningless
+    if snapnum == 8 or snapnum == 13:
+        add_formation_time = np.nan * np.ones(data['halo_M_Mean200'].shape)
+    else:
+        add_formation_time = []
+        for idx in tqdm(subset_idx):
+            find_idx = np.where(idx_table.loc[f'snap_{snapnum}'] == idx)[0]
+
+            if len(find_idx) == 0:
+                # print('no halo found')
+                add_formation_time.append([np.nan])
+            else:
+                # Check if mass matches
+                if np.all(np.abs(mass_table.loc[f'snap_{snapnum}'].iloc[find_idx].to_numpy() - snap_all_mass[idx]) < 0.01):
+                    # print('mass matches')
+                    # Compute the formation time
+                    match_zs = compute_formation_time(mass_table.iloc[:,find_idx], snapnum, z_dict)
+                    mean_z = np.mean(match_zs)
+                    add_formation_time.append([mean_z])
+                else:
+                    # print('mass does not match')
+                    exit()
+        add_formation_time = np.concatenate(add_formation_time)
+        
+    print(add_formation_time.shape, data['halo_M_Mean200'].shape)
+    
+    # Add formation time to halo data
+    # -------------------------------------------------------------------------------------------------
+    data['formation_time'] = add_formation_time
+    print(data.keys())
+    np.save(f'{halos_dir}/{halos_fname}', data)
+    print('data saved')
+    
+def compute_formation_time(mass, snapnum, z_dict):
+    
+    """
+    Find the row index in `column_mass` where the column values are equal to 
+    half of the values in row `snap_{snapnum}`.
+    
+    Parameters:
+    mass : pd.DataFrame
+        A DataFrame where rows are indexed by 'snap_{snapnum}' format.
+    snapnum : int
+        The current snapshot.
+    
+    Returns:
+    dict
+        A dictionary where keys are column names and values are lists of row indices
+        that satisfy the condition.
+    """
+    import numpy as np
+
+    half_mass = mass.loc[f'snap_{snapnum}'] / 2  # Compute half mass for each column
+    abs_diffs = np.abs(mass - half_mass)
+    match_snaps = abs_diffs.idxmin().values # [list of snaps]
+    match_zs = [z_dict[snap] for snap in match_snaps]
+    return match_zs
